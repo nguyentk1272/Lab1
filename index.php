5ab71b492a98b08eaaef7dc6b583933e8990e81b
nguyentk1272 <80952748+nguyentk1272@users.noreply.github.com>
2021-03-19 17:08:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Test2</title>
+		<link rel="stylesheet" type="text/css" href="Test2.css">
+	</head>
+	<body>
+ 	 <nav id="topmenu">
+  		<ul>
+   		 <li><a href="#">Home</a></li>
+    	<li><a href="#">Menu</a></li>
+    	<li><a href="#">Courses</a></li>
+ 		 </ul>
+ 	 </nav>
+ 	 <section>
+ 	 <h1>Betta fish</h1>
+ 	 <img src="fish.jpg" style="width:400px;height:350px;" />
+	</section>
+	<section>
+  <article>
+    <header>
+      <h1>Quéo cum các bạn đến zới mai qép sai</h1>
+      <p>Written by Nguyên</p>
+    </header>
+    <p>This is my phớt bờ lốc</p>
+  </article>
+</section>
+<nav>
+  <ul>
+    <li><a href=" https://www.facebook.com/nguyentk1272/">My FaceBook</a></li>
+  </ul>
+</nav>
+<section>
+  <article>chủ đề hôm nay là cc gì đó đéo biết nữa :)))  </article>
+</section>
+<p>The Samurai Betta fish</p>
+<figure>
+  <img src="samuraibettafish.jpg" alt="The su" style="width:450px;height:350px;">
+  <figcaption>Fig.1 - The Samurai Betta fish </figcaption>
+</figure>
+<audio controls autoplay>
+  <source src="nhac2.mp3" type="audio/mpeg">
+</audio>
+<address>
+  Street Address: 27 Nguyễn Bá Ngọc, An Sơn, Tam Kỳ, Quảng Nam<br/>
+  Country Name: Việt Nam<br/>
+  Tel: +84 086 828 5424
+</address>
+	
+
+<footer>
+  <p>&copy; Copyright 2009 …</p>
+</footer>
+	</body>
+</html> 
